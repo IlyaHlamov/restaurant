eae22e5d7eff6ac9aeb8cba159e3be64dbefba76
IlyaHlamov <ilyahlamov58@gmail.com>
2025-05-13 17:20:31 +0300

    some somes

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'restaurant-movt.onrender.com',
+    'localhost',
+    '127.0.0.1']
 
 
 # Application definition
